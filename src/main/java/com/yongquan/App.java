da91dadb7f55d93514d6a3367cf053bc22536259
guoliang <zhou-guo-liang@pacific-textiles.com>
2018-06-09 10:32:53 +0800

    test debug level

--- src/main/java/com/yongquan/App.java
+++ src/main/java/com/yongquan/App.java
@@ -1,32 +1,24 @@
 package com.yongquan;
 
 import org.apache.log4j.Logger;
+public class App {
 
-public class App{
- 
-  final static Logger logger = Logger.getLogger(App.class);
- 
-  public static void main(String[] args) {
- 
-    App obj = new App();
-    obj.runMe("w3cschool");
- 
-  }
- 
-  private void runMe(String parameter){
- 
-    if(logger.isDebugEnabled()){
-      logger.debug("This is debug : " + parameter);
-    }
- 
-    if(logger.isInfoEnabled()){
-      logger.info("This is info : " + parameter);
-    }
- 
-    logger.warn("This is warn : " + parameter);
-    logger.error("This is error : " + parameter);
-    logger.fatal("This is fatal : " + parameter);
- 
-  }
- 
-}
+	  final static Logger logger = Logger.getLogger(App.class);
+
+	  public static void main(String[] args) {
+
+	    App obj = new App();
+
+	    try {
+	      obj.divide();
+	    } catch (ArithmeticException ex) {
+	      logger.error("Sorry, something wrong!", ex);
+	    }
+	  }
+
+	  private void divide() {
+	    int i = 10 / 0;
+
+	  }
+
+	}
