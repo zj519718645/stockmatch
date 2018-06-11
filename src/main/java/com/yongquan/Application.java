af41f8ba310831870759df67c8e5418dbdf5a63f
guoliang <zhou-guo-liang@pacific-textiles.com>
2018-06-11 15:44:03 +0800

    Fix compile error

--- src/main/java/com/yongquan/Application.java
+++ src/main/java/com/yongquan/Application.java
@@ -19,7 +19,7 @@ public class Application {
 		return args -> {
 			System.out.println("Let's inspect the beans provided by Spring Boot:");
 			
-			String[] beanNames = ctx.getBeanDefinitionsNames();
+			String[] beanNames = ctx.getBeanDefinitionNames();
 			Arrays.sort(beanNames);
 			for (String beanName : beanNames) {
 				System.out.println(beanName);
