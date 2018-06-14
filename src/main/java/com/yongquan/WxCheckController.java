bb5b3444bddfabd2d7d89cb556d91ca7cfbf4d2d
guoliang <zhou-guo-liang@pacific-textiles.com>
2018-06-14 15:47:55 +0800

    add dependency for Encry

--- src/main/java/com/yongquan/WxCheckController.java
+++ src/main/java/com/yongquan/WxCheckController.java
@@ -1,7 +1,5 @@
 package com.yongquan;
 
-import cn.com.bonc.wx.service.WxService;  
-import cn.com.bonc.wx.util.SignUtil;  
 import org.springframework.beans.factory.annotation.Autowired;  
 import org.springframework.web.bind.annotation.RequestMapping;  
 import org.springframework.web.bind.annotation.RequestMethod;  
@@ -11,16 +9,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;  
 import java.io.IOException;  
 import java.io.PrintWriter;  
+import net.binjoo.utils.SHA1;
   
 @RestController  
 @RequestMapping("/getResult")  
 public class WxCheckController {  
-  
-    @Autowired  
-    private WxService wxUserService;  
+
   
   
-  
+	private String TOKEN = "LiangSheng998_LoveKunan1314";
     /** 
      * 与公众号服务器配置进行比对 
      * @param req 
@@ -32,25 +29,18 @@ public class WxCheckController {
         String signature = req.getParameter("signature");  
         // 时间戳  
         String timestamp = req.getParameter("timestamp");  
+
         // 随机数  
         String nonce = req.getParameter("nonce");  
         // 随机字符串  
         String echostr = req.getParameter("echostr");  
   
-  
-        PrintWriter out = null;  
-        try {  
-            out = res.getWriter();  
-            // 通过检验signature对请求进行校验，若校验成功则原样返回echostr，否则接入失败  
-            if (SignUtil.checkSignature(signature, timestamp, nonce)) {  
-                out.print(echostr);  
-            }  
-        } catch (IOException e) {  
-            e.printStackTrace();  
-        } finally {  
-            out.close();  
-            out = null;  
-        }  
+        String[] str = { TOKEN, timestamp, nonce };
+
+        String bigStr = str[0] + str[1] + str[2];
+        
+        String digest = new SHA1().getDigestOfString(bigStr.getBytes()).toLowerCase();
+        
         System.out.println(req);  
     }  
   
